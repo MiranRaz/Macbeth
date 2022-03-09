ab30c1ce8061e78b23d22e9b2155ab1174f4f9f5
miranraznatovic <miran.raznatovic@edu.fit.ba>
2022-03-09 14:58:40 +0100

    loader

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
-
 import Main from "./components/main/Main";
 import NavBar from "./components/navbar/NavBar";
 import Stream from "./components/stream/Stream";
@@ -8,16 +7,36 @@ import History from "./components/history/History";
 import SideBar from "./components/sidebar/SideBar";
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="app">
-      <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <SideBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <div className="sections">
-        <Main />
-        <Stream />
-        <History />
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <div className="loadCont">
+          <h1>Macbeth</h1>
+        </div>
+      ) : (
+        <div className="app">
+          <>
+            <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+            <SideBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+            <div className="sections">
+              <Main />
+              <Stream />
+              <History />
+            </div>
+          </>
+        </div>
+      )}
+    </>
   );
 }
