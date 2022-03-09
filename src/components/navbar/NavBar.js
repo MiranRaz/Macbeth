6dc3caf681c9a1d3a1bd196109ab415a3a8e1d1c
miranraznatovic <miran.raznatovic@edu.fit.ba>
2022-03-09 06:30:45 +0100

    macbeth webpage

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="left">Sta ima lijevo</div>
+      <div className="right">Sta ima desno</div>
+    </div>
+  );
+}
