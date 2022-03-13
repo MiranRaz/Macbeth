fcd4f14751eb310fe9fad4f068d0ca795cd91541
miranraznatovic <miran.raznatovic@edu.fit.ba>
2022-03-13 15:48:19 +0100

    loader

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
+
 import Gallery from "./components/gallery/Gallery";
 import NavBar from "./components/navbar/NavBar";
 import SideBar from "./components/sidebar/SideBar";
 
+import gitara from "../src/img/gitara.png";
+
 export default function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <div className="app">
-      <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <SideBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <div className="sections">
-        <Gallery />
-      </div>
+      {loading ? (
+        <div className="loadingData">
+          <div className="icona">
+            <img src={gitara} />
+          </div>
+        </div>
+      ) : (
+        <>
+          <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <SideBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <div className="sections">
+            <Gallery />
+          </div>
+        </>
+      )}
     </div>
   );
 }
